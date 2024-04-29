c53eae8cf3952d822b54fbb11db363ede62b5948
ES0543 <ercemolaa@gmail.com>
2024-04-29 16:13:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Firebase projenizi başlatın
-firebase.initializeApp(firebaseConfig);
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAZoVTonJU6t4Z_ih5yG8t2Rv5B9PW6xGM",
-  authDomain: "bosstimer-d5faf.firebaseapp.com",
-  projectId: "bosstimer-d5faf",
-  storageBucket: "bosstimer-d5faf.appspot.com",
-  messagingSenderId: "650333902048",
-  appId: "1:650333902048:web:2b9811cdf9dc6bd160ec85",
-  measurementId: "G-5XVTW7LTFD"
-};
 function addMarker(event) {
     var message = document.getElementById('message').value;
     var dot = document.createElement('div');
