2d7a9912fc6bb86807f9f122877ade61e517c296
ES0543 <ercemolaa@gmail.com>
2024-04-29 16:47:55 +0300

    Update firebase-app.js

--- firebase-app.js
+++ firebase-app.js
@@ -1,1 +1,13 @@
-
+<script src="https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js"></script>
+<script>
+  // Firebase yapılandırma bilgileri
+  const firebaseConfig = {
+    apiKey: "AIzaSyAZoVTonJU6t4Z_ih5yG8t2Rv5B9PW6xGM",
+    authDomain: "bosstimer-d5faf.firebaseapp.com",
+    projectId: "bosstimer-d5faf",
+    storageBucket: "bosstimer-d5faf.appspot.com",
+    messagingSenderId: "650333902048",
+    appId: "1:650333902048:web:2b9811cdf9dc6bd160ec85",
+    measurementId: "G-5XVTW7LTFD"
+  };
